36db28325938264c51f5589d9dc23b4bc115bd2a
Christof Pieloth <dorfboy3@aol.com>
2011-06-18 19:49:55 +0200

    Hadoop and OpenCL linking: Hadoop Pipes

---
+++ hadoop_ocl_link_test/src/pipes/mrSimple.cpp
@@ -0,0 +1,41 @@
+#include <string>
+
+#include  <stdint.h>  // <-- this is missing from the book
+
+#include "hadoop/Pipes.hh"
+#include "hadoop/TemplateFactory.hh"
+
+class MR_SimpleMapper : public HadoopPipes::Mapper
+{
+public:
+
+  MR_SimpleMapper(HadoopPipes::TaskContext& context) {}
+
+  void map(HadoopPipes::MapContext& context)
+  {
+    std::string line = context.getInputValue();
+    line.append("_map_called");
+    context.emit(context.getInputKey(), line);
+  }
+
+};
+
+class MR_SimpleReducer : public HadoopPipes::Reducer
+{
+public:
+
+  MR_SimpleReducer(HadoopPipes::TaskContext& context) {}
+
+  void reduce(HadoopPipes::ReduceContext& context)
+  {
+    std::string line = context.getInputValue();
+    line.append("_reduce_called");
+    context.emit(context.getInputKey(), line);
+  }
+
+};
+
+int main(int argc, char* argv[])
+{
+  return HadoopPipes::runTask(HadoopPipes::TemplateFactory<MR_SimpleMapper, MR_SimpleReducer>());
+}
