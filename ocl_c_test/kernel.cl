0107e45416d95466553d5df469d773eaf4d038d0
Christof Pieloth <dorfboy3@aol.com>
2011-06-22 23:09:14 +0200

    Finish MaxValue example.

--- ocl_c_test/kernel.cl
+++ ocl_c_test/kernel.cl
@@ -1,64 +1,28 @@
-__kernel void addVec(__global int* vecC, const __global int* vecA, const __global int* vecB, const unsigned int size) {
-	unsigned int w = get_global_id(0);
-	if(w >= size)
-		return;
-	vecC[w] = vecA[w] + vecB[w];
-}
-
-__kernel void matMulSingle(__global int* matC, const unsigned int mC, const unsigned int nC,
-		const __global int* matA, const unsigned int mA, const unsigned int nA,
-		const __global int* matB, const unsigned int mB, const unsigned int nB) {
-	unsigned int w = get_global_id(0);
-	unsigned int c = w % nC;
-	unsigned int r = (w - c) / nC;
-
-	if(w >= mC * nC)
-	  return;
-
-	int tmp = 0;
-
-	for (size_t j = 0; j < nA && j < mB; ++j)
-    {
-		tmp += matA[r * nA + j] *matB[j * nB + c];
-	}
-
-	matC[w] = tmp;
-}
-
-// unfinished
-__kernel void maxInt(__global int* values, const unsigned int SIZE, const unsigned int CHK_SIZE,  __local int* localValues)
+__kernel void
+maxInt(__global int* values, const unsigned int SIZE, __local int* localValues)
 {
   int maxVal = -2147483648;
-  //for (uint y = get_group_id(0); y < SIZE; y += get_num_groups(0))
-  {
-      localValues[get_local_id(0)] = values[get_local_id(0)];
 
-      for (uint stride = ceil(get_local_size(0)/2); stride > 0; stride = ceil(stride/2))
-      {
-          barrier(CLK_LOCAL_MEM_FENCE);
+  const unsigned int LSIZE = get_local_size(0);
+  const unsigned int LID = get_local_id(0);
 
-          if (get_local_id(0) < stride && get_local_id(0)+stride < get_local_size(0))
-            localValues[get_local_id(0)] = max(localValues[get_local_id(0)], localValues[get_local_id(0) + stride]);
-      }
+  localValues[LID] = values[get_global_id(0)];
 
-      if (get_local_id(0) == 0)
-        values[get_group_id(0)] = localValues[0];
+  barrier(CLK_LOCAL_MEM_FENCE);
+
+  unsigned int stride = LSIZE;
+  do
+    {
+      stride = convert_uint(ceil(convert_float(stride) / 2));
+
+      if (LID < stride && (LID + stride) < LSIZE)
+        localValues[LID] = max(localValues[LID], localValues[LID + stride]);
 
       barrier(CLK_LOCAL_MEM_FENCE);
-  }
+    }
+  while (stride > 1);
 
-
-  barrier(CLK_GLOBAL_MEM_FENCE);
-
-  if(get_global_id(0) == 0)
-  {
-          maxVal = values[0];
-          for(uint i = 1; i < get_num_groups(0); i++)
-          {
-              maxVal = max(maxVal, values[i]);
-          }
-          values[0] = maxVal;
-  }
+  if (get_local_id(0) == 0)
+    values[get_group_id(0)] = localValues[0];
 
 }
-
--- ocl_c_test/maxValue.cpp
+++ ocl_c_test/maxValue.cpp
@@ -6,14 +6,16 @@
 #define __CL_ENABLE_EXCEPTIONS
 #endif
 
-#define X_DIM 0
-#define Y_DIM 1
+#define PRT_CNT 8
+#define WG_FAC 64
 
 #include "CL/cl.hpp"
 
 #include <iostream>
+#include <sstream>
 #include <fstream>
 #include <algorithm>
+#include <math.h>
 
 #include "logging.hpp"
 #include "timeUtils.hpp"
@@ -37,6 +39,17 @@ const char TIMEL = 't';
 const logging::Level TIME("TIME", 32);
 
 std::string
+getString(int* vec, const size_t SIZE)
+{
+  std::stringstream sstream;
+  for (size_t i = 0; i < SIZE; i++)
+    {
+      sstream << vec[i] << ", ";
+    }
+  return sstream.str();
+}
+
+std::string
 readFile(std::string fName)
 {
   std::ifstream file;
@@ -54,60 +67,26 @@ readFile(std::string fName)
   return kernelSrc;
 }
 
-unsigned int
-ggt(unsigned int a, unsigned int b)
-{
-  unsigned int c;
-  do
-    {
-      c = a % b;
-      a = b;
-      b = c;
-    }
-  while (c != 0);
-  return a;
-}
-
 // TODO Berechnung fuer optimale Work-Group-Size optimieren
 void
-calcWorkGroupSize(size_t dim, size_t* globalSize, size_t* localSize,
+calcWorkGroupSize(size_t& globalSize, size_t& localSize,
     const size_t MAX_GROUP_SIZE)
 {
-  switch (dim)
-    {
-  case 1:
-    if (globalSize[X_DIM] < localSize[X_DIM])
-      localSize[X_DIM] = globalSize[X_DIM];
-    else
-      localSize[X_DIM] = ggt(globalSize[X_DIM], localSize[X_DIM]);
-    return;
-
-  case 2:
-    if (globalSize[X_DIM] < localSize[X_DIM])
-      localSize[X_DIM] = globalSize[X_DIM];
-    if (globalSize[Y_DIM] < localSize[Y_DIM])
-      localSize[Y_DIM] = globalSize[Y_DIM];
-    while (MAX_GROUP_SIZE < (localSize[X_DIM] * localSize[Y_DIM])
-        || (globalSize[X_DIM] % localSize[X_DIM] != 0) || (globalSize[Y_DIM]
-        % localSize[Y_DIM] != 0))
-      {
-        --localSize[X_DIM];
-        --localSize[Y_DIM];
-      }
-    return;
-
-  default:
-    return;
-    }
+  localSize = MAX_GROUP_SIZE;
+  if (globalSize < localSize)
+    localSize = globalSize;
+  else
+    while (globalSize % localSize != 0)
+      --localSize;
 }
 
 double
-maxValue(int* values, const size_t SIZE)
+maxValue(int* values, const size_t LEN)
 {
   timeUtils::Clock timer;
   timer.start();
 
-  for (size_t i = 1; i < SIZE; i++)
+  for (size_t i = 1; i < LEN; i++)
     values[0] = std::max(values[0], values[i]);
 
   timer.stop();
@@ -119,17 +98,16 @@ initCL(cl::Context & context, const cl_device_type CL_TYPE,
     std::vector<cl::Device> & devices, cl::CommandQueue & cmdQ,
     cl::Program & program, cl::Kernel & kernel)
 {
-  /*** Initialisiere OpenCL-Objekte ***/
-
   /*** Erstelle OpenCL-Context und CommandQueue ***/
   context = cl::Context(CL_TYPE);
   devices = context.getInfo<CL_CONTEXT_DEVICES> ();
-  //device = devices.front();
   cmdQ = cl::CommandQueue(context, devices[0], CL_QUEUE_PROFILING_ENABLE);
+
   /*** OpenCL-Quellcode einlesen ***/
   std::string src = readFile(KERNEL_PATH);
   cl::Program::Sources source;
   source.push_back(std::make_pair(src.data(), src.length()));
+
   /*** OpenCL-Programm aus Quellcode erstellen ***/
   program = cl::Program(context, source);
   try
@@ -150,10 +128,11 @@ initCL(cl::Context & context, const cl_device_type CL_TYPE,
 }
 
 double
-maxValueCL(const cl_device_type CL_TYPE, int* values, const size_t SIZE)
+maxValueCL(const cl_device_type CL_TYPE, int* values, size_t len)
 {
   const std::string METHOD("maxValueCL");
   timeUtils::Clock timer;
+  double runtimeKernel = 0;
 
   std::vector<cl::Platform> platforms;
   std::vector<cl::Device> devices;
@@ -193,7 +172,7 @@ maxValueCL(const cl_device_type CL_TYPE, int* values, const size_t SIZE)
       /*** Erstellen und Vorbereiten der Daten ***/
       timer.start();
       cl::Buffer vBuffer(context, CL_MEM_READ_WRITE | CL_MEM_COPY_HOST_PTR,
-          sizeof(cl_int) * SIZE, &values[0], &status);
+          sizeof(cl_int) * len, &values[0], &status);
       if (status != CL_SUCCESS)
         {
           throw cl::Error(status, "cl::Buffer values");
@@ -205,49 +184,65 @@ maxValueCL(const cl_device_type CL_TYPE, int* values, const size_t SIZE)
       // Work-Items pro Work-Group = localSize
       const size_t MAX_GROUP_SIZE = devices[0].getInfo<
           CL_DEVICE_MAX_WORK_GROUP_SIZE> ();
-      size_t globalSize[1] =
-        { SIZE };
-      size_t localSize[1] =
-        { MAX_GROUP_SIZE };
-      calcWorkGroupSize(1, globalSize, localSize, MAX_GROUP_SIZE);
-      Logger::logDebug(METHOD,
-          Logger::sStream << "globalSize[0]: " << globalSize[0]);
-      Logger::logDebug(METHOD,
-          Logger::sStream << "localSize[0]: " << localSize[0]);
+      size_t globalSize;
+      size_t localSize;
 
-      /*** Kernel-Argumente setzen  ***/
-      status = kernel.setArg(0, vBuffer);
-      if (status != CL_SUCCESS)
+      do
         {
-          throw cl::Error(status, "Kernel.SetArg");
-        }
-      status = kernel.setArg(1, (cl_uint)(SIZE));
-      if (status != CL_SUCCESS)
-        {
-          throw cl::Error(status, "Kernel.SetArg");
-        }
-      status = kernel.setArg(2, (cl_uint)(localSize[0]));
-      if (status != CL_SUCCESS)
-        {
-          throw cl::Error(status, "Kernel.SetArg");
-        }
+          globalSize = len;
+          calcWorkGroupSize(globalSize, localSize, MAX_GROUP_SIZE);
+          if (localSize == 1)
+            {
+              globalSize = ceil((double) len / WG_FAC) * WG_FAC;
+              calcWorkGroupSize(globalSize, localSize, MAX_GROUP_SIZE);
+              Logger::logDebug(
+                  METHOD,
+                  Logger::sStream << "GlobalSize has been extended to "
+                      << globalSize);
+            }
 
-      status = kernel.setArg(3, sizeof(cl_int) * localSize[0], NULL);
-      if (status != CL_SUCCESS)
-        {
-          throw cl::Error(status, "Kernel.SetArg");
+          Logger::logDebug(METHOD,
+              Logger::sStream << "globalSize: " << globalSize);
+          Logger::logDebug(METHOD,
+              Logger::sStream << "localSize: " << localSize);
+
+          /*** Kernel-Argumente setzen  ***/
+          status = kernel.setArg(0, vBuffer);
+          if (status != CL_SUCCESS)
+            {
+              throw cl::Error(status, "Kernel.SetArg");
+            }
+          status = kernel.setArg(1, (cl_uint)(len));
+          if (status != CL_SUCCESS)
+            {
+              throw cl::Error(status, "Kernel.SetArg");
+            }
+
+          status = kernel.setArg(2, sizeof(cl_int) * localSize, NULL);
+          if (status != CL_SUCCESS)
+            {
+              throw cl::Error(status, "Kernel.SetArg");
+            }
+
+          /*** Kernel ausfuehren und auf Abarbeitung warten ***/
+
+          cl::KernelFunctor func = kernel.bind(cmdQ, cl::NDRange(globalSize),
+              cl::NDRange(localSize));
+
+          cl::Event event = func();
+
+          cmdQ.finish();
+
+          runtimeKernel += event.getProfilingInfo<CL_PROFILING_COMMAND_END> ();
+          runtimeKernel
+              -= event.getProfilingInfo<CL_PROFILING_COMMAND_START> ();
+
+          len = globalSize / localSize;
         }
-
-      /*** Kernel ausfuehren und auf Abarbeitung warten ***/
-      cl::KernelFunctor func = kernel.bind(cmdQ, cl::NDRange(globalSize[0]),
-          cl::NDRange(localSize[0]));
-
-      cl::Event event = func();
-
-      cmdQ.finish();
+      while (globalSize > localSize && localSize > 1);
 
       /*** Daten vom OpenCL-Device holen ***/
-      status = cmdQ.enqueueReadBuffer(vBuffer, true, 0, sizeof(cl_int) * SIZE,
+      status = cmdQ.enqueueReadBuffer(vBuffer, true, 0, sizeof(cl_int) * len,
           &values[0]);
       if (status != CL_SUCCESS)
         {
@@ -256,9 +251,6 @@ maxValueCL(const cl_device_type CL_TYPE, int* values, const size_t SIZE)
 
       timer.stop();
 
-      double runtimeKernel = 0;
-      runtimeKernel += event.getProfilingInfo<CL_PROFILING_COMMAND_END> ();
-      runtimeKernel -= event.getProfilingInfo<CL_PROFILING_COMMAND_START> ();
       Logger::log(METHOD, TIME,
           Logger::sStream << "timeKernel=" << 1.0e-9 * runtimeKernel << ";");
     }
@@ -289,7 +281,7 @@ checkArguments(int argc, char** argv)
 {
   if (argc < 4)
     {
-      std::cout << "Argumente: " << ALL << "|" << DEBUG << "|" << NORMAL << "|"
+      std::cout << "Arguments: " << ALL << "|" << DEBUG << "|" << NORMAL << "|"
           << TIMEL << "|" << ERRORL << " " << SINGLE << "|" << CPU << "|"
           << GPU << " <size>" << std::endl;
       return false;
@@ -334,17 +326,29 @@ main(int argc, char** argv)
 
   Logger::logInfo(METHOD, Logger::sStream << "type = " << type);
   Logger::logInfo(METHOD, Logger::sStream << "size = " << size);
+
+  /*** Erzeugen der Daten***/
+  if (size % WG_FAC != 0)
+    {
+      size = ceil((double) size / WG_FAC) * WG_FAC;
+      Logger::logWarn(METHOD,
+          Logger::sStream << "Array has been extended to " << size);
+    }
+
   Logger::logInfo(METHOD,
       Logger::sStream << "RAM (KB) > " << (size * sizeof(int)) / 1024);
 
-  /*** Erzeugen der Daten***/
   int* values = (int*) malloc(size * sizeof(int));
 
   fillVector(values, size);
 
+  /*** Kontrollwerte ***/
+  values[(size_t) (size / 3)] = 2323;
   values[(size_t) (size / 2)] = 4242;
   values[size - 1] = 7331;
-  values[(size_t) (size / 3)] = 2323;
+
+  Logger::logInfo(METHOD,
+      Logger::sStream << getString(values, size > PRT_CNT ? PRT_CNT : size));
 
   /*** Implementierung waehlen ***/
   switch (type)
@@ -361,15 +365,14 @@ main(int argc, char** argv)
   default:
     ;
     Logger::logError(METHOD,
-        Logger::sStream << "Falsches Argument \"" << type << "\"");
+        Logger::sStream << "Unknown type: \"" << type << "\"");
     }
 
-  // ACHTUNG getMatrixString wuerde immer berechnet werden! (bad_alloc moeglich)
-  // if (runtime > -1 && (result.rows < 10 && result.cols < 10))
-  Logger::log(METHOD, TIME, Logger::sStream << "time=" << runtime << ";");
+  if (runtime > -1)
+    Logger::logInfo(METHOD,
+        Logger::sStream << getString(values, size > PRT_CNT ? PRT_CNT : size));
 
-  for (size_t i = 0; i < 15 && i < size; i++)
-    std::cout << values[i] << std::endl;
+  Logger::log(METHOD, TIME, Logger::sStream << "time=" << runtime << ";");
 
   free(values);
 
