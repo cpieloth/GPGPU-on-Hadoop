4c16c312e7ca0d94be15d1fb22d854e0ff33ddb4
Christof Pieloth <dorfboy3@aol.com>
2011-06-24 00:35:10 +0200

    Fix DataSet class.

--- hadoop_ocl_link_test/src/gsod/DataSet.java
+++ hadoop_ocl_link_test/src/gsod/DataSet.java
@@ -2,8 +2,8 @@ package gsod;
 
 public class DataSet {
 
-	public final static int YEAR_START = 15;
-	public final static int YEAR_END = 18;
+	public final static int YEAR_START = 15-1;
+	public final static int YEAR_END = 18-1;
 
 	public final static int TEMP_START = 25;
 	public final static int TEMP_END = 30;
@@ -26,11 +26,15 @@ public class DataSet {
 	}
 
 	private static int getInt(String line, final int START, final int END) {
-		// parseInt doesn't like leading plus signs
-		if (line.charAt(87) == '+')
-			return Integer.parseInt(line.substring(START + 1, END + 1));
-		else
-			return Integer.parseInt(line.substring(START, END + 1));
+		line = line.replace('*', ' ');
+		return (int)(Double.parseDouble(line.substring(START, END + 1)) * 10);
+	}
+
+	public static void main(String[] args) {
+		String line = "010010 99999  20081220    24.6 24    18.5 24   997.8 24   996.6 24    5.7  6   15.6 24   27.2  999.9    29.8*   20.7*  0.01G 999.9  001000";
+		System.out.println(DataSet.getYear(line));
+		System.out.println(DataSet.getTemp(line));
+		System.out.println(DataSet.getMax(line));
 	}
 
 }
