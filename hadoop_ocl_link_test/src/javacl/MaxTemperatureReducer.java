66d875567bab3a0770090ab5ff237fdea69296bc
Christof Pieloth <dorfboy3@aol.com>
2011-06-24 00:37:25 +0200

    Continue work on MaxTemperature OpenCL implementation (unfinished).

--- hadoop_ocl_link_test/src/javacl/MaxTemperatureReducer.java
+++ hadoop_ocl_link_test/src/javacl/MaxTemperatureReducer.java
@@ -1,39 +1,196 @@
 package javacl;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.IntBuffer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.EnumSet;
 import java.util.Iterator;
+import java.util.Scanner;
 
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
+import com.nativelibs4java.opencl.CLBuffer;
+import com.nativelibs4java.opencl.CLContext;
+import com.nativelibs4java.opencl.CLDevice;
+import com.nativelibs4java.opencl.CLDevice.QueueProperties;
+import com.nativelibs4java.opencl.CLEvent;
+import com.nativelibs4java.opencl.CLException;
+import com.nativelibs4java.opencl.CLKernel;
+import com.nativelibs4java.opencl.CLKernel.LocalSize;
+import com.nativelibs4java.opencl.CLMem;
+import com.nativelibs4java.opencl.CLPlatform;
+import com.nativelibs4java.opencl.CLProgram;
+import com.nativelibs4java.opencl.CLQueue;
+import com.nativelibs4java.opencl.JavaCL;
+
 public class MaxTemperatureReducer extends
 		Reducer<Text, IntWritable, Text, IntWritable> {
 
-	private final int MAX_VALUES = 65535;	// max memory of opencl device
-	
+	private final int MAX_VALUES = 65536; // max memory of opencl device,
+											// MAX_VALUE % 64 = 0
+
+	private static final String KERNEL_PATH = "src/kernel.cl";
+	private static final int WG_FAC = 64;
+
+	// TODO fehlberechnung auf gpu
 	public void reduce(Text key, Iterable<IntWritable> values, Context context)
 			throws IOException, InterruptedException {
 		int[] tmpValues = new int[MAX_VALUES];
-		tmpValues[0] = Integer.MIN_VALUE;	// temporary max temp
+		tmpValues[0] = Integer.MIN_VALUE; // temporary max temp
 		int i = 1;
-		
-		for(Iterator<IntWritable> it = values.iterator(); it.hasNext();) {
+
+		for (Iterator<IntWritable> it = values.iterator(); it.hasNext();) {
 			// fill array to copy on opencl device, mind max memory of device!
-			if(i < MAX_VALUES)
+			if (i < MAX_VALUES) {
 				tmpValues[i++] = it.next().get();
+			}
 			// if max values or no more values to add, start opencl kernel
-			if(i >= MAX_VALUES || !it.hasNext()) {
-				tmpValues[0] = getMaxTempOCL(tmpValues, i);
+			if (i >= MAX_VALUES || !it.hasNext()) {
+				tmpValues[0] = getMaxIntOCL(tmpValues, i);
 				i = 1;
+				System.out.println("max: " + tmpValues[0] + " - key: " + key);
 			}
 		}
 
 		context.write(key, new IntWritable(tmpValues[0]));
 	}
 
-	private int getMaxTempOCL(int[] tmpValues, int i) {
-		// TODO Auto-generated method stub
-		return 0;
+	private int getMaxIntOCL(int[] values, int size) {
+		System.out.println("getMaxIntOCL called. value.length=" + values.length
+				+ " size=" + size);
+		int EXIT_FAILURE = Integer.MIN_VALUE;
+		final CLDevice.Type CL_TYPE = CLDevice.Type.GPU;
+
+		// calculate optimal length
+		int offset = size;
+		if (size < values.length && size % WG_FAC != 0) {
+			size = (int) Math.ceil((double) size / WG_FAC) * WG_FAC;
+			size = size < values.length ? size : values.length;
+			System.out.println("resized to optimal start size " + size);
+		}
+		// fill end of array with minimum
+		for (int i = offset; i < size; i++)
+			values[i] = Integer.MIN_VALUE;
+
+		// FIXME wrong maxInt
+		try {
+			// Init OpenCL
+			CLPlatform[] platforms = JavaCL.listPlatforms();
+
+			EnumSet<CLDevice.Type> types = EnumSet.of(CL_TYPE);
+			ArrayList<CLDevice> devices = new ArrayList<CLDevice>();
+			CLDevice[] devTmp;
+
+			for (CLPlatform platform : platforms) {
+				devTmp = platform.listDevices(types, true);
+				devices.addAll(Arrays.asList(devTmp));
+			}
+
+			devTmp = new CLDevice[devices.size()];
+			CLContext context = JavaCL.createContext(null,
+					devices.toArray(devTmp));
+			CLQueue cmdQ = context
+					.createDefaultQueue(QueueProperties.ProfilingEnable);
+
+			String src = readFile(KERNEL_PATH);
+
+			CLProgram program = context.createProgram(src);
+
+			try {
+				program.build();
+			} catch (Exception err) {
+				System.out.println("Build log for \"" + devices.get(0) + "\n"
+						+ err.getMessage());
+				return EXIT_FAILURE;
+			}
+
+			CLKernel kernel = program.createKernel("maxInt");
+
+			// Prepate Data
+			CLBuffer<IntBuffer> vBuffer = context.createBuffer(
+					CLMem.Usage.InputOutput, IntBuffer.wrap(values, 0, size),
+					true);
+
+			cmdQ.finish();
+
+			final long MAX_GROUP_SIZE = devices.get(0).getMaxWorkGroupSize();
+			int globalSize;
+			int localSize;
+
+			do {
+				globalSize = size;
+				localSize = calcWorkGroupSize(globalSize, MAX_GROUP_SIZE);
+				System.out.println("GlobalSize: " + globalSize);
+				System.out.println("LocalSize: " + localSize);
+				if (localSize == 1) {
+					globalSize = (int) (Math.ceil((double) size / WG_FAC) * WG_FAC);
+					localSize = calcWorkGroupSize(globalSize, MAX_GROUP_SIZE);
+					System.out.println("GlobalSize has been extended to "
+							+ globalSize);
+					System.out.println("LocalSize has been changed to "
+							+ localSize);
+				}
+
+				kernel.setArg(0, vBuffer);
+				kernel.setArg(1, new LocalSize(localSize));
+
+				// Run kernel
+				// CLEvent event =
+				kernel.enqueueNDRange(cmdQ, new int[] { globalSize },
+						new int[] { localSize }, new CLEvent[0]);
+
+				cmdQ.finish();
+
+				size = globalSize / localSize;
+			} while (globalSize > localSize && localSize > 1);
+
+			// Get results
+			IntBuffer resBuffer = ByteBuffer
+					.allocateDirect(values.length * Integer.SIZE)
+					.order(context.getByteOrder()).asIntBuffer();
+			vBuffer.read(cmdQ, resBuffer, true, new CLEvent[0]);
+			resBuffer.rewind();
+			resBuffer.get(values);
+
+			return values[0];
+		} catch (CLException err) {
+			System.out.println("OpenCL error:\n" + err.getMessage() + "():"
+					+ err.getCode());
+			err.printStackTrace();
+			return EXIT_FAILURE;
+		} catch (Exception err) {
+			System.out.println("Error:\n" + err.getMessage() + "()");
+			err.printStackTrace();
+			return EXIT_FAILURE;
+		}
+	}
+
+	private int calcWorkGroupSize(int globalSize, final long MAX_GROUP_SIZE) {
+		int localSize = (int) MAX_GROUP_SIZE;
+		if (globalSize < localSize)
+			localSize = globalSize;
+		else
+			while (globalSize % localSize != 0)
+				--localSize;
+		return localSize;
+	}
+
+	private static String readFile(String fName) {
+		StringBuffer sb = new StringBuffer();
+		try {
+			Scanner sc = new Scanner(new File(fName));
+			while (sc.hasNext())
+				sb.append(sc.nextLine());
+
+		} catch (Exception e) {
+			System.out.println("Could not read file: " + fName);
+			e.printStackTrace();
+		}
+		return sb.toString();
 	}
 }
--- hadoop_ocl_link_test/src/kernel.cl
+++ hadoop_ocl_link_test/src/kernel.cl
@@ -1,53 +1,25 @@
-/* avoiding wrong highlighting in IDEs, delete it before compiling */
-#define __kernel
-#define __global
-#define __local
-
-__kernel void addVec(__global int* vecC, const __global int* vecA, const __global int* vecB, const unsigned int size)
+__kernel void maxInt(__global int* values, __local int* localValues)
 {
-	unsigned int w = get_global_id(0);
-	if(w >= size)
-		return; 
-	vecC[w] = vecA[w] + vecB[w];
-}
+  const unsigned int LSIZE = get_local_size(0);
+  const unsigned int LID = get_local_id(0);
 
-#define MIN_TEMP -512
+  localValues[LID] = values[get_global_id(0)];
 
-/* __local int* localTemp -> temp of each work group kernel.setArg(..., sizeof(cl_int) * WORK_GROUP_SIZE, NULL); */
-__kernel void max(__global int* values, const int SIZE, const int CHK_SIZE, __local int* localTemp)
-{
-	// Each work-group computes multiple elements of W
-	for (uint y = get_group_id(0); y < SIZE; y += get_num_groups(0))
-	{
-		int temp = MIN_TEMP;
-		for (uint x = get_local_id(0); x < CHK_SIZE; x += get_local_size(0))
-			temp = max(temp, values[x]);
+  barrier(CLK_LOCAL_MEM_FENCE);
 
-		localTemp[get_local_id(0)] = temp;
+  unsigned int stride = LSIZE;
+  do
+    {
+      stride = convert_uint(ceil(convert_float(stride) / 2));
 
-		// work-items are gathering and calculating the dotProduct
-		for (uint stride = get_local_size(0)/2; stride > 0; stride /= 2)
-		{
-			barrier(CLK_LOCAL_MEM_FENCE);
+      if (LID < stride && (LID + stride) < LSIZE)
+        localValues[LID] = max(localValues[LID], localValues[LID + stride]);
 
-			if (get_local_id(0) < stride)
-				localTemp[get_local_id(0)] = max(localTemp[get_local_id(0)], localTemp[get_local_id(0) + stride]);
-		}
+      barrier(CLK_LOCAL_MEM_FENCE);
+    }
+  while (stride > 1);
 
-		if (get_local_id(0) == 0)
-			values[get_group_id(0)] = localTemp[0];
+  if (get_local_id(0) == 0)
+    values[get_group_id(0)] = localValues[0];
 
-		barrier(CLK_LOCAL_MEM_FENCE);
-	}
-
-	// TODO put in parallel reduction
-	int maxTemp = values[0];
-	if(get_global_id(0) == 0)
-	{
-		for(uint i = 1; i < get_num_groups(0); i++)
-		{
-			maxTemp = max(maxTemp, values[i]);
-		}
-		values[0] = maxTemp;
-	}
 }
