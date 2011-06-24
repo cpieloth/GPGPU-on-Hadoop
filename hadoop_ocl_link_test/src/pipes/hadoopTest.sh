13edfcd36ee2c0034f5bd6d36211b09acf706af0
Christof Pieloth <dorfboy3@aol.com>
2011-06-24 17:24:14 +0200

    Finish Hadoop Pipes example.

--- hadoop_ocl_link_test/src/pipes/hadoopTest.sh
+++ hadoop_ocl_link_test/src/pipes/hadoopTest.sh
@@ -2,14 +2,30 @@
 make clean
 make
 
-HADOOP_HOME="/opt/hadoop"
-EXEC_HOME="/home/christof/Dokumente/GPGPU-on-Hadoop/github/hadoop_ocl_link_test/src/pipes"
-ARGS="-input ${HADOOP_HOME}/input/vectors.txt -output ${HADOOP_HOME}/output -program ${EXEC_HOME}/mrSimple"
+# Hadoop Pipes require HDFS
 
-# echo $HADOOP_HOME
-# echo $EXEC_HOME
-# echo $ARGS
+HADOOP_HOME="/opt/hadoop"
+EXEC_LOCAL="/Users/christof/Documents/GPGPU-on-Hadoop/github/hadoop_ocl_link_test/src/pipes/mrSimple"
+EXEC_DFS="/exec/mrSimple"
+INPUT_LOCAL="${HADOOP_HOME}/input"
+INPUT_DFS="/input"
+OUTPUT_LOCAL="${HADOOP_HOME}/output_pipes"
+OUTPUT_DFS="/output_pipes"
+ARGS="-input $INPUT_DFS -output $OUTPUT_DFS -program $EXEC_DFS"
+OPTS="-D hadoop.pipes.java.recordreader=true -D hadoop.pipes.java.recordwriter=true"
 
 cd $HADOOP_HOME
-rm -f -r output
-bin/hadoop pipes $ARGS
+
+bin/start-all.sh
+
+bin/hadoop dfs -rmr $EXEC_DFS
+bin/hadoop dfs -rmr $INPUT_DFS
+bin/hadoop dfs -rmr $OUTPUT_DFS
+
+bin/hadoop fs -put $EXEC_LOCAL $EXEC_DFS
+bin/hadoop fs -put $INPUT_LOCAL $INPUT_DFS
+
+bin/hadoop pipes $OPTS $ARGS
+
+echo "Output:"
+bin/hadoop dfs -cat ${OUTPUT_DFS}/part*
