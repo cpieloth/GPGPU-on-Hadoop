737ecca58c105876d542aa3e2547bce1c6e1d9e4
Christof Pieloth <dorfboy3@aol.com>
2011-09-22 18:35:16 +0200

    Add new information.

--- cl_info/src/javal_cl/CLInfo.java
+++ cl_info/src/javal_cl/CLInfo.java
@@ -15,29 +15,47 @@ public class CLInfo {
 			/*** Hole OpenCL-Plattformen z.B. AMD APP, NVIDIA CUDA ***/
 			CLPlatform[] platforms = JavaCL.listPlatforms();
 			System.out.println("Platforms:");
-			for(CLPlatform pf : platforms) {
+			for (CLPlatform pf : platforms) {
 				System.out.println("\tName: " + pf.getName());
 				System.out.println("\tVendor: " + pf.getVendor());
 				System.out.println("\tVersion: " + pf.getVersion());
 				System.out.println();
 				System.out.println("\tDevices:");
-				for(CLDevice dev : pf.listAllDevices(false)) {
+				for (CLDevice dev : pf.listAllDevices(false)) {
 					System.out.println("\t\tName: " + dev.getName());
+					System.out.println("\t\tVendor: " + dev.getVendor());
 					System.out.println("\t\tDriver: " + dev.getDriverVersion());
-					System.out.println("\t\tOpenCL Version: " + dev.getOpenCLVersion());
+					System.out.println("\t\tOpenCL Version: "
+							+ dev.getOpenCLVersion());
 					System.out.println("\t\tType: " + dev.getType());
-					System.out.println("\t\tCompute Units: " + dev.getMaxComputeUnits());
-					System.out.println("\t\tGlobal Memory Size: " + dev.getGlobalMemSize());
-					System.out.println("\t\tWork Group Size: " + dev.getMaxWorkGroupSize());
-					System.out.println("\t\tWork Item Size[Dim-0]: " + dev.getMaxWorkItemSizes()[0]);
-					System.out.println("\t\tExtensions: " + getString(dev.getExtensions()));
+					System.out.println("\t\tClock Frequency: "
+							+ dev.getMaxClockFrequency());
+					System.out.println("\t\tCompute Units: "
+							+ dev.getMaxComputeUnits());
+					System.out.println("\t\tGlobal Memory Size: "
+							+ dev.getGlobalMemSize());
+					System.out.println("\t\tMemory Alloc Size: "
+							+ dev.getMaxMemAllocSize());
+					System.out.println("\t\tConstant Memory Size: "
+							+ dev.getMaxConstantBufferSize());
+					System.out.println("\t\tLocal Memory Size: "
+							+ dev.getLocalMemSize());
+					System.out.println("\t\tWork Group Size: "
+							+ dev.getMaxWorkGroupSize());
+					System.out.println("\t\tWork Item Dimension: "
+							+ dev.getMaxWorkItemDimensions());
+					for (int i = 0; i < dev.getMaxWorkItemDimensions(); i++)
+						System.out.println("\t\t\tWork Item Size[" + i + "]: "
+								+ dev.getMaxWorkItemSizes()[i]);
+					System.out.println("\t\tExtensions: "
+							+ getString(dev.getExtensions()));
 					System.out.println();
 				}
 			}
-			
+
 		} catch (CLException err) {
-			System.out.println("OpenCL error:\n" + err.getMessage()
-					+ "():" + err.getCode());
+			System.out.println("OpenCL error:\n" + err.getMessage() + "():"
+					+ err.getCode());
 			err.printStackTrace();
 		} catch (Exception err) {
 			System.out.println("Error:\n" + err.getMessage() + "()");
@@ -45,10 +63,10 @@ public class CLInfo {
 		}
 
 	}
-	
+
 	private static String getString(String[] values) {
 		StringBuilder sb = new StringBuilder();
-		for(String s : values)
+		for (String s : values)
 			sb.append(s + " ");
 		return sb.toString();
 	}
